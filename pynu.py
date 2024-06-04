c0d0fc5681a4dd942855b98e5b98bedc2334023e
ArturRegadas <ahlregadas@gmail.com>
2024-06-03 21:08:59 -0300

    1.1

--- pynu.py
+++ pynu.py
@@ -1,14 +1,18 @@
-from pynput.keyboard import Key, Listener
+from pynput.keyboard import Key, Listener, KeyCode
+start_stop_key = KeyCode(char="p")
+stop_key = KeyCode(char="es")
 
 def on_press(key):
-    try:
-        print('Tecla {0} pressionada'.format(key.char))
-    except AttributeError:
-        print('Tecla especial {0} pressionada'.format(key))
+    if key == start_stop_key:
+        print("e")
+        return True
+        
 
 def on_release(key):
-    if key == Key.esc:
+    if key == stop_key:
+        print("esc")
         return False
+        
 
 # Inicia o listener
 with Listener(on_press=on_press, on_release=on_release) as listener:
